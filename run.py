003f01541c820c9d6314bfe8959f9d3358f2cbc6
Rohit Kumar <rohitcoder@Rohits-MacBook-Pro.local>
2022-08-31 00:19:39 +0530

    Added support big collections

--- run.py
+++ run.py
@@ -6,8 +6,11 @@ sourcedb = sourceCluster["source_cluster_name_here"]
 targetCluster = MongoClient("Connection_STRING_HERE")
 targetdb = targetCluster["source_cluster_name_here"]
 
+
 # list all collections and copy it to target cluster one by one
 for collection in sourcedb.list_collection_names():
     print("Copying collection: " + collection)
-    targetdb[collection].insert_many(sourcedb[collection].find())
+    ## DOWNLOAD IN grouping of 5000 documents to avoid memory error and use insert_many()
+    for doc in sourcedb[collection].find({}).batch_size(5000):
+        targetdb[collection].insert_one(doc)
     print("\nCollection: " + collection + " copied to target cluster")
